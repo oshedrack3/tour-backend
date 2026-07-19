e345bb76d5208a3a7fcc862bbb40c44dfae1498c
oshedrack3 <oshedrack3@gmail.com>
2026-07-19 09:40:39 +0100

    added logo support and security check for admin

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(
-    "Server running on port " + PORT
+    "Server currently running on port " + PORT
   );
 });
 
