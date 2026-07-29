e61e381546e65647f2900001cb03c21445c8c647
oshedrack3 <oshedrack3@gmail.com>
2026-07-29 16:28:47 +0100

    fixed payload limit

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ setInterval(() => {
 }, 25000);
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({
+  limit: "25mb"
+}));
+
 
 app.use("/auth", authRoutes);
 app.use("/tournaments", tournamentRoutes);
