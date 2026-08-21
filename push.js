486d8f98713a5de549365ce31947287d869a38d6
oshedrack3 <oshedrack3@gmail.com>
2026-08-21 14:33:48 +0100

    update push.js

--- push.js
+++ push.js
@@ -1,5 +1,30 @@
 const webpush = require("web-push");
 
+console.log(
+  "VAPID_PUBLIC_KEY exists:",
+  !!process.env.VAPID_PUBLIC_KEY
+);
+
+console.log(
+  "VAPID_PUBLIC_KEY length:",
+  process.env.VAPID_PUBLIC_KEY?.length
+);
+
+console.log(
+  "VAPID_PRIVATE_KEY exists:",
+  !!process.env.VAPID_PRIVATE_KEY
+);
+
+console.log(
+  "VAPID_PRIVATE_KEY length:",
+  process.env.VAPID_PRIVATE_KEY?.length
+);
+
+console.log(
+  "VAPID_SUBJECT:",
+  process.env.VAPID_SUBJECT
+);
+
 webpush.setVapidDetails(
   process.env.VAPID_SUBJECT,
   process.env.VAPID_PUBLIC_KEY,
