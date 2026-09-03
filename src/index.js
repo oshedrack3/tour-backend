ebafdc61a4f7a79c9c932dae2536d3d7ad4f10c0
oshedrack3 <oshedrack3@gmail.com>
2026-09-03 22:11:18 +0100

    update index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import {
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
   "Access-Control-Max-Age": "86400"
 };
